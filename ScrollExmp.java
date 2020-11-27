5202b08f4a68f38ba6f169c16ad4f3803e2c5427
Nadeem012 <55021916+ndm81@users.noreply.github.com>
2020-11-27 23:43:33 +0530

    Add files via upload

---
+++ ScrollExmp.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+class ScrollExmp{
+	ScrollExmp(){
+		Frame f=new Frame();
+		Scrollbar s=new Scrollbar();
+		s.setBounds(100,100,50,100);
+		f.add(s);
+		f.setSize(400,400);
+		f.setLayout(null);
+		f.setVisible(true);
+		
+	}
+	public static void main(String[] args) {
+		new ScrollExmp();
+	}
+}
